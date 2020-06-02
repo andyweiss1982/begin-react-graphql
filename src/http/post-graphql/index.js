9eb8d67bfa21d94e3ff65de9c087c93ed1fc7984
Andy Weiss <andyweiss1982@gmail.com>
2020-06-02 12:28:26 -0400

    sign up and log in

--- src/http/post-graphql/index.js
+++ src/http/post-graphql/index.js
@@ -1,7 +1,12 @@
 const arc = require("@architect/functions");
 const { ApolloServer, gql } = require("apollo-server-lambda");
 const data = require("@begin/data");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
 const table = "tasks";
+const secret = "secret";
+const saltRounds = 10;
 
 const typeDefs = gql`
   type Task {
@@ -9,10 +14,21 @@ const typeDefs = gql`
     description: String!
     createdAt: String!
   }
+  type User {
+    key: String!
+    password: String!
+    tasks: [Task]!
+  }
+  type JWT {
+    token: String!
+  }
   type Query {
+    currentUser: User
     tasks: [Task]!
   }
   type Mutation {
+    signup(email: String!, password: String!): JWT
+    login(email: String!, password: String!): JWT
     createTask(description: String!): Task!
     deleteTask(key: ID!): Task!
   }
@@ -24,6 +40,27 @@ const resolvers = {
       (await data.get({ table })).sort((a, b) => b.createdAt - a.createdAt),
   },
   Mutation: {
+    signup: async (_, { email, password }) => {
+      const existingUser = await data.get({ table: "users", key: email });
+      if (existingUser) return;
+      const hash = bcrypt.hashSync(password, saltRounds);
+      const newUser = await data.set({
+        table: "users",
+        key: email,
+        password: hash,
+        tasks: [],
+      });
+      const token = jwt.sign(newUser, secret);
+      return { token };
+    },
+    login: async (_, { email, password }) => {
+      const user = await data.get({ table: "users", key: email });
+      if (!user) return;
+      const match = bcrypt.compareSync(password, user.password);
+      if (!match) return;
+      const token = jwt.sign(user, secret);
+      return { token };
+    },
     createTask: async (_, { description }) =>
       await data.set({ table, description, createdAt: +Date.now() }),
     deleteTask: async (_, { key }) => {
@@ -34,12 +71,29 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ event }) => {
+    const token = event.headers.authorization || "";
+    let email;
+    try {
+      email = jwt.verify(token, secret).email || "";
+    } catch (error) {
+      email = "";
+    }
+    let user = await data.get({ table: "users", key: email });
+    // data returns an empty array when email is empty
+    if (Array.isArray(user) && !user.length) user = null;
+    if (user) delete user.password;
+    return { user };
+  },
+});
+
 const handler = server.createHandler();
 
 exports.handler = function (event, context, callback) {
   const body = arc.http.helpers.bodyParser(event);
-  // Body is now parsed, re-encode to JSON for Apollo
   event.body = JSON.stringify(body);
   handler(event, context, callback);
 };
